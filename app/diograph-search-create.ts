35be50da66a9a3449af3fa08499c352ec2bb922a
Jouni Alanen <jvalanen@gmail.com>
2017-07-08 21:34:21 +0300

    First try with <diograph-search-create>

---
+++ app/diograph-search-create.ts
@@ -0,0 +1,86 @@
+
+import * as $ from "jquery";
+import * as Bloodhound from "corejs-typeahead/dist/bloodhound";
+import "corejs-typeahead/dist/typeahead.jquery";
+
+export class DiographSearchCreate extends HTMLElement {
+
+  attachedCallback() {
+    this.initializeTypeahead();
+  }
+
+  // Fires when an instance of the element is created.
+  createdCallback() {
+    this.innerHTML =  `
+      <link href="main.css" rel="stylesheet">
+
+      <div id="search-create">
+        <input class="typeahead" type="text" placeholder="Search for diories...">
+        <div id='loading-icon'><img src='loading.gif'></div>
+        <div class='search-create__cancel'>X</div>
+      </div>
+    `;
+  };
+
+  initializeTypeahead() {
+    var searchResults = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      remote: {
+        url: 'http://diory-server.herokuapp.com/v1/search',
+        prepare: function(query, settings) {
+          settings.url = settings.url + '?q=' + query,
+          settings.headers = {
+            "Authorization": "test-token"
+          };
+          return settings;
+        }
+      }
+    });
+
+    $('#search-create .typeahead').typeahead({
+        hint: false,
+        minLength: 3
+      }, {
+        display: 'value',
+        source: searchResults,
+        templates: {
+          suggestion: function(value) {
+            return `
+              <div class="search-result diory-element__element">
+                  <div class="diory-element__title">
+                    ${value.value}
+                  </div>
+              </div>`
+          },
+          empty: function() {
+            return `<div class="search-result diory-element__element">
+                      <div class="diory-element__title">No results.</div>
+                    </div>`
+          }
+        }
+    });
+
+    // X click
+    $('.search-create__cancel').click(() => {
+      $('.typeahead').typeahead('val', '');
+    })
+
+    // Loading starts
+    $('#search-create').bind('typeahead:asyncrequest', function() {
+      $('#loading-icon').css('display', 'block');
+    });
+
+    // Loading ends
+    $('#search-create').bind('typeahead:asyncreceive', function() {
+      $('#loading-icon').hide();
+    });
+
+    // Loadinc cancelled (esc)
+    $('#search-create').bind('typeahead:asynccancel', function() {
+      $('#loading-icon').hide();
+    });
+
+  }
+
+}
