02f725384e6a70d873b997f6ccaf517417921c5f
Jouni Alanen <jvalanen@gmail.com>
2017-07-08 21:48:00 +0300

    Fine tuning + main.css moved inline

--- app/index.ts
+++ app/index.ts
@@ -1,8 +1,7 @@
 require('./index.html');
-require('./main.css');
 require('./loading.gif');
 
 import 'document-register-element'
 import { DiographSearchCreate } from "./diograph-search-create"
 
-(<any>document).registerElement('diograph-search-create', DiographSearchCreate);
+(document as any).registerElement('diograph-search-create', DiographSearchCreate);
